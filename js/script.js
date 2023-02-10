1c87b6c5313934c4b1c1d781c0e08095931c4d1b
Zhanarys <92988527+zhvnvrys@users.noreply.github.com>
2023-02-11 00:07:07 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false,
+  ride: "carousel",
+  pause: hover
+})
+  
